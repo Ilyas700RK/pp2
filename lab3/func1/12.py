ffc498f23693ba2bc1287200e8df4013c7a171c9
Ilyas700RK <123076961+Ilyas700RK@users.noreply.github.com>
2024-02-15 16:40:37 +0600

    Delete lab3 directory

--- lab3/func1/12.py
+++
@@ -1,9 +0,0 @@
-# 12 task
-def histogram(list):
-    for i in list:
-        k=i
-        for j in range(k):
-            print('*',sep='',end='')
-        print()
-
-# histogram([4, 9, 7])
--- lab3/func1/6.py
+++
@@ -1,9 +0,0 @@
-# 6th task
-
-def reverses(str):
-    return str[::-1]
-
-# b=input().split()
-# print(*reverses(b))
-
-# ----------------------------------
